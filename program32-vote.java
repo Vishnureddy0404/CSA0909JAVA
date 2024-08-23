bea39aa62b81bd900e9f3a399cbb9d78d8692e14
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-08-23 09:19:07 +0530

    program32-vote.java

--- program32-vote.java
+++ program32-vote.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 public class vote{
   public static void main(String[] args){
     Scanner a = new Scanner(System.in);
-    
+    System.out.print("enter the age:");
     int l = 18;
     int n = a.nextInt();
     int b = l - n;
-    
     if(l == n){
       System.out.println("eligible for vote");
     }
