864c2f08f618a81fc4b7adb2ab6323009c638095
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-08-26 10:16:23 +0530

    update program 52-bubble sort.java

---
+++ program 52-bubble sort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class R192211820 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        scanner.close();
+    }
+}
