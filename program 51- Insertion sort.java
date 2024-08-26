ffc43d5c0953347217b73c7b4eb4b5245f089efb
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-08-26 10:15:40 +0530

    update program 51- Insertion sort.java

---
+++ program 51- Insertion sort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class R192211820 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the n : ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println(n +"Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        scanner.close();
+    }
+}
