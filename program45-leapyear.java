5d1a758f94e6966a23b4c6346efa8d8e0321d8fb
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-08-26 09:53:58 +0530

    Update program45-leapyear.java

--- program45-leapyear.java
+++ program45-leapyear.java
@@ -1,11 +1,19 @@
-public class leap{
-  public static void main(String[] args){
-    int n = 2023;
-    if(n%4 == 0){
-      System.out.print("it is leap year");
+import java.util.Scanner;
+
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        // Check if the year is a leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
     }
-    else{
-      System.out.print("it not a leap year");
-    }
-  }
 }
