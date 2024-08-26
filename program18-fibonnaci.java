0a37e45527fe0cdab93c30680e0517ba9430b9b6
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-08-26 10:43:06 +0530

    Update program18-fibonnaci.java

--- program18-fibonnaci.java
+++ program18-fibonnaci.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
-
-public class Fibonacci {
+public class R192211820 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of terms: ");
-        int n = scanner.nextInt();
-
-        int firstTerm = 0, secondTerm = 1;
-
-        System.out.print("Fibonacci Series: " + firstTerm + ", " + secondTerm);
-
-        for (int i = 3; i <= n; i++) {
-            int nextTerm = firstTerm + secondTerm;
-            System.out.print(", " + nextTerm);
-
-            firstTerm = secondTerm;
-            secondTerm = nextTerm;
+        System.out.print("Enter the maximum value for the Fibonacci series:\n");
+        int maxNumber = scanner.nextInt();
+        if (maxNumber < 0) {
+            System.out.println("The maximum value must be a non-negative integer.");
+        } else {
+            int a = 0; 
+            int b = 1;
+            System.out.println("Fibonacci series up to " + maxNumber + ":");
+            if (maxNumber >= a) {
+                System.out.print(a + " ");
+            }
+            if (maxNumber >= b) {
+                System.out.print(b + " ");
+            }
+            int nextNumber;
+            while (true) {
+                nextNumber = a + b;
+                if (nextNumber > maxNumber) {
+                    break;
+                }
+                System.out.print(nextNumber + " ");
+                a = b;
+                b = nextNumber;
+            }
         }
+        scanner.close();
     }
 }
